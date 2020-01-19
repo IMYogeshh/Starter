523cbb6b38508097bd8ea4ddb93315dd4fd4e0b2
yogesh.pawar01@vidushigoc.com <yogesh.pawar01@vidushigoc.com>
2020-01-19 17:59:33 +0530

    Added Validation For radio buttons

--- form/src/app/personalinfo/personalinfo.component.ts
+++ form/src/app/personalinfo/personalinfo.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 export class PersonalinfoComponent implements OnInit {
   model: any = {}
   country: any = ['America', 'India', 'England']
+
+  titleSt = ['MR.', 'MRS.', 'MISS.']
   ngOnInit() {
   }
   mySub(frm: NgForm) {
